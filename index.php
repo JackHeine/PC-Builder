4bb1447f525e78d5f03229e0a82e8dcbf07066ae
Gavin Hulvey <gavin.hulvey@gmail.com>
2023-04-03 19:38:41 -0400

    Starter PHP

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+    // Include the header
+    include 'header.php';
+?>
+
+    <div class="container">
+        <!-- Content here -->
+        <h1>Homepage</h1>
+        <p>Home page stuff goes here</p>
+        <p>This project uses Bootstrap for classes</p>
+    </div>
+
+<?php
+    // Include the footer
+    include 'footer.php';
+?>
+
+    
+
+   
